b043fe98bfbc101553981349d9cc41c83c9b5507
Emanuele Tagliamonte <emanuele.s.tagliamonte@gmail.com>
2026-08-15 13:37:22 +0200

    Some Updates to the Map Asset Editor, including the removal of Barrel-related scripts and assets, and modifications to core game files and scene management.

--- Assets/MapAssetEditor/EVS/BarrelEventListener.cs
+++
@@ -1,9 +0,0 @@
-using LaniakeaCode.Utilities;
-
-namespace LaniakeaCode.Events
-{
-    public class BarrelEventListener : BaseGameEventListener<BarrelTypeData, BarrelEvent, UnityBarrelEvent>
-    {
-
-    }
-}
--- Assets/MapAssetEditor/EVS/UnityBarrelEvent.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine.Events;
-using LaniakeaCode.Utilities;
-
-namespace LaniakeaCode.Events
-{
-    [System.Serializable] public class UnityBarrelEvent : UnityEvent<BarrelTypeData> { }
-}
